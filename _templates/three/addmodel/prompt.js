a6ffe806b5ba4cd79eeed80dcb4cdd26eb1ca4f1
Fadhil <81182492+FadParatlas@users.noreply.github.com>
2022-10-03 03:05:02 +0800

    added template system

---
+++ _templates/three/addmodel/prompt.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+      type: 'input',
+      name: 'filename',
+      message: "What's your filename?"
+    },
+
+    {
+      type: 'input',
+      name: 'animname',
+      message: "What's your animation name?"
+    },
+
+  ]
